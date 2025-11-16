31a81ef5767a3618e5343f744b3385d4da231c8f
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-17 01:15:39 +0900

    🐛 Fix: svg 컴포넌트 설정 수정

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,19 +1,19 @@
-import { ReactComponent as ArrowDownIcon } from './ic-arrow-down.svg';
-import { ReactComponent as ArrowLeftIcon } from './ic-arrow-left.svg';
-import { ReactComponent as ArrowRightIcon } from './ic-arrow-right.svg';
-import { ReactComponent as BackIcon } from './ic-back.svg';
+import ArrowDownIcon from './ic-arrow-down.svg';
+import ArrowLeftIcon from './ic-arrow-left.svg';
+import ArrowRightIcon from './ic-arrow-right.svg';
+import BackIcon from './ic-back.svg';
 import facebookIcon from './ic-facebook.svg?url';
-import { ReactComponent as HeartActiveIcon } from './ic-heart-active.svg';
-import { ReactComponent as HeartIcon } from './ic-heart.svg';
+import HeartActiveIcon from './ic-heart-active.svg';
+import HeartIcon from './ic-heart.svg';
 import instgramIcon from './ic-instagram.svg?url';
-import { ReactComponent as KebabIcon } from './ic-kebab.svg';
-import { ReactComponent as PlusIcon } from './ic-plus.svg';
-import { ReactComponent as RemoveIcon } from './ic-remove.svg';
-import { ReactComponent as SearchIcon } from './ic-search.svg';
-import { ReactComponent as SortIcon } from './ic-sort.svg';
+import KebabIcon from './ic-kebab.svg';
+import PlusIcon from './ic-plus.svg';
+import RemoveIcon from './ic-remove.svg';
+import SearchIcon from './ic-search.svg';
+import SortIcon from './ic-sort.svg';
 import twitterIcon from './ic-twitter.svg?url';
-import { ReactComponent as HidePasswordIcon } from './ic-visibility-off.svg';
-import { ReactComponent as ShowPasswordIcon } from './ic-visibility-on.svg';
+import HidePasswordIcon from './ic-visibility-off.svg';
+import ShowPasswordIcon from './ic-visibility-on.svg';
 import youtubeIcon from './ic-youtube.svg?url';
 
 export const icons = {
--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,8 +1,4 @@
 declare module '*.svg' {
-  import * as React from 'react';
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  const src: string;
-  export default src;
+  const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default value;
 }
