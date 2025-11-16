d5a3c76a591bbadef579fd0386206702090b408a
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-16 22:53:16 +0900

    ⚙️ Chore: 린트 tseslint 설정 추가

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules']),
@@ -15,6 +16,7 @@ export default defineConfig([
     files: ['**/*.{js,jsx,ts,tsx}'],
     extends: [
       js.configs.recommended,
+      tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
       componentArrowStyle.configs.recommended,
