79ce97d94cfae8de494857f756f2f88962fe091e
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-17 00:11:48 +0900

    🐛 Fix: Link 태그 a태그로 변경

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -16,27 +16,27 @@ const Footer = () => {
           </Link>
         </div>
         <div className={styles['sns-area']}>
-          <Link
+          <a
             href="https://www.facebook.com/codeit.kr/"
             target="_blank"
             rel="noopener noreferrer">
             <img src={icons.facebookIcon} alt="페이스북" />
-          </Link>
-          <Link href="https://x.com/" target="_blank" rel="noopener noreferrer">
+          </a>
+          <a href="https://x.com/" target="_blank" rel="noopener noreferrer">
             <img src={icons.twitterIcon} alt="X(트위터)" />
-          </Link>
-          <Link
+          </a>
+          <a
             href="https://www.youtube.com/channel/UCCM79CPm2WbBYTRaiNEExbg"
             target="_blank"
             rel="noopener noreferrer">
             <img src={icons.youtubeIcon} alt="유튜브" />
-          </Link>
-          <Link
+          </a>
+          <a
             href="https://www.instagram.com/codeit_kr/"
             target="_blank"
             rel="noopener noreferrer">
             <img src={icons.instgramIcon} alt="인스타그램" />
-          </Link>
+          </a>
         </div>
       </div>
     </footer>
