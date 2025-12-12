122e383add44e7a2e91bda2da7b837fbe4cbe32a
aahreum <cocoding420@gmail.com>
2025-12-13 01:05:43 +0900

    ✨ Feat: TodoListBadge 컴포넌트 추가

---
+++ src/features/todo/ui/todo-list/TodoListBadge.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@/shared/lib/cn';
+
+interface TodoListBadgeProps {
+  status: 'TO DO' | 'DONE';
+}
+
+export default function TodoListBadge({ status }: TodoListBadgeProps) {
+  return (
+    <span
+      className={cn(
+        'inline-flex h-[36px] items-center rounded-full px-[27px] font-accent text-lg',
+        {
+          'bg-lime-300 text-green-700': status === 'TO DO',
+          'bg-green-700 text-amber-300': status === 'DONE',
+        }
+      )}>
+      {status}
+    </span>
+  );
+}
