f10f979a2db45f4c67509442a177a53928af196f
aahreum <cocoding420@gmail.com>
2025-12-12 23:33:22 +0900

    🎨 Style: 공통 스타일 유틸리티로 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
       </head>
       <body>
         <Header />
-        <main>{children}</main>
+        <main className='mt-[24px] px-[16px]'>
+          <div className='container'>{children}</div>
+        </main>
       </body>
     </html>
   );
