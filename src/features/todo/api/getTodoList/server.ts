ccbbc3bbf9fd4059139fc22ec2109378a094ff07
aahreum <cocoding420@gmail.com>
2025-12-14 02:30:21 +0900

    ♻️ Refactor: server api, client api 분리

--- src/features/todo/api/getTodoList/server.ts
+++ src/features/todo/api/getTodoList/server.ts
@@ -1,12 +1,6 @@
-import { clientApi } from '@/shared/api/client';
 import { serverApi } from '@/shared/api/server';
 
 export const getTodoListServer = async () => {
   const res = await serverApi.get('/items');
-  return res.data;
-};
-
-export const getTodoList = async () => {
-  const res = await clientApi.get('/items');
   return res.data;
 };
