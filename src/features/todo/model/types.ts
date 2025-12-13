d6e0321e9544fec90665e042ad0a22ab900d76a7
aahreum <cocoding420@gmail.com>
2025-12-14 02:59:02 +0900

    ⚙️ Chore: 타입 추가

--- src/features/todo/model/types.ts
+++ src/features/todo/model/types.ts
@@ -1,1 +1,9 @@
 export type TodoListStatus = 'TO DO' | 'DONE';
+
+export interface TodoList {
+  id: number;
+  name: string;
+  isCompleted: boolean;
+}
+
+export type TodoListResponse = TodoList[];
