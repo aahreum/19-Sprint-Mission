c9ec070dcf73f04ce5d6acd4dabad0643b1c5a55
aahreum <cocoding420@gmail.com>
2025-12-14 03:41:05 +0900

    ⚙️ Chore: 타입 추가

--- src/features/todo/model/types.ts
+++ src/features/todo/model/types.ts
@@ -14,3 +14,12 @@ export interface UpdateTodoReqBody {
   imageUrl?: string;
   isCompleted?: boolean;
 }
+
+export interface UpdateTodoResponse {
+  name: string;
+  memo: string;
+  imageUrl: string;
+  isCompleted: boolean;
+  id: number;
+  tenantId: number;
+}
